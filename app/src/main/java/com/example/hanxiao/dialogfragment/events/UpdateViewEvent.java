cc0a9be7dc20bfa2d7d4f434a9edb2bcd77512d2
wenzhi <wenzhi@mogujie.com>
2017-04-21 10:06:18 +0800

    add project

---
+++ app/src/main/java/com/example/hanxiao/dialogfragment/events/UpdateViewEvent.java
@@ -0,0 +1,16 @@
+package com.example.hanxiao.dialogfragment.events;
+
+/**
+ * Created by wenzhi on 17/4/20.
+ */
+
+public class UpdateViewEvent<T> {
+    public int whichChild;//更新那个子页面
+    public String arg1;//提供预置的简单参数便于视图更新
+    public String arg2;
+    public T obj;
+
+    public UpdateViewEvent(int whichChild) {
+        this.whichChild = whichChild;
+    }
+}
