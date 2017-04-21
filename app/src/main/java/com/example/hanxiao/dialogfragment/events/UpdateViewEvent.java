270abd48e2edc3e00632d3bc51dd7ea9750c421d
wenzhi <wenzhi@mogujie.com>
2017-04-21 14:02:35 +0800

    update UpdateViewEvent: add flag

--- app/src/main/java/com/example/hanxiao/dialogfragment/events/UpdateViewEvent.java
+++ app/src/main/java/com/example/hanxiao/dialogfragment/events/UpdateViewEvent.java
@@ -8,6 +8,8 @@ public class UpdateViewEvent<T> {
     public int whichChild;//更新那个子页面
     public String arg1;//提供预置的简单参数便于视图更新
     public String arg2;
+    public boolean flag1;//提供预置的boolean标识判断
+    public boolean flag2;
     public T obj;
 
     public UpdateViewEvent(int whichChild) {
